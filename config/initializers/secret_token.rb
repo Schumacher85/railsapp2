b1162ec0da6c834a81478555142f1dbcf2be0e6b
Christian Schumacher <SchumacherChristian85@web.de>
2012-05-02 20:31:00 +0200

    initalcomit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarkmanager::Application.config.secret_token = '31dc2b5a96ccf33df8a9b39fff9697c3433629181903e3b9a4b823e38ad2cf4539e85f76c263dd3694d82f66574b556b9149e6c2e2abbc532cb0688157bc773c'
